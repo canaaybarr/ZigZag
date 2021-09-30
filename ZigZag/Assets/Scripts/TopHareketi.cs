bd1b4d76ec45d30a127b854fc2aef40996c893fd
canaaybarr <78181891+canaaybarr@users.noreply.github.com>
2021-09-30 11:07:58 +0300

    unity

---
+++ ZigZag/Assets/Scripts/TopHareketi.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TopHareketi : MonoBehaviour
+{
+    Vector3 yön;
+    public float hız;
+    public ZeminSpawner zeminspawnerscripti;
+    public static bool düştü_mü;
+    public float eklenecekhız;
+    void Start()
+    {
+        yön = Vector3.forward;
+        düştü_mü = false;
+    }
+
+  
+    void Update()
+    {
+        if(transform.position.y<= 0.5f)
+        {
+            düştü_mü = true;
+        }
+        if(düştü_mü == true)
+        {
+            Debug.Log("düştüm");
+            return;
+        }
+        if(Input.GetMouseButtonDown(0))
+        {
+            if(yön.x == 0)
+            {
+                yön = Vector3.left;
+            }
+            else
+            {
+                yön = Vector3.forward;
+            }
+
+            hız = hız + eklenecekhız * Time.deltaTime;
+        }
+    }
+    private void FixedUpdate() 
+    {
+        Vector3 hareket = yön*Time.deltaTime*hız;
+        transform.position += hareket;
+    }
+    private void OnCollisionExit(Collision collision) 
+    {
+        if(collision.gameObject.tag == "zemin")
+        {
+            Skor.skor++;
+            collision.gameObject.AddComponent<Rigidbody>();
+            zeminspawnerscripti.zemin_oluştur();
+            StartCoroutine(ZeminSil(collision.gameObject));
+        }
+    }
+    IEnumerator ZeminSil(GameObject SilinecekZemin)
+    {
+        yield return new WaitForSeconds(3f);
+        Destroy(SilinecekZemin);
+    }
+}
